3b8c7b0a6096537748b14f399c7520584e43ac47
royston-corda <roystoncorda@gmail.com>
2023-12-08 14:25:03 -0500

    work:app and lib commits

---
+++ libraries/my-controls/lib/MyClass.js
@@ -0,0 +1,10 @@
+var MyClass = /** @class */ (function () {
+    function MyClass() {
+    }
+    MyClass.prototype.doSomething = function () {
+        console.log('Hello, world!');
+    };
+    return MyClass;
+}());
+export { MyClass };
+//# sourceMappingURL=MyClass.js.map
